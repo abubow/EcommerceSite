49249e183c725b6734ba4a34ebb5ef93f47264d7
smainc <40357673+smainc@users.noreply.github.com>
2022-05-15 13:06:51 -0700

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -31,10 +31,11 @@ const Arrow = styled.div`
 `;
 
 const Wrapper = styled.div`
-height: 100%;
+    height: 100%;
 `;
 
 const Slide = styled.div`
+    display:flex;
     align-items: center;
     justify-content: center;
     width: 100vw;
@@ -45,7 +46,6 @@ const Slide = styled.div`
 const ImgContainer = styled.div`
     flex: 1;
     background-color: #fafafa;
-    width: 100%;
     height: 100%;
 `;
 const Image = styled.img`
@@ -55,7 +55,23 @@ const Image = styled.img`
 
 const InfoContainer = styled.div`
     flex: 1;
+    padding 50px;
 `;
+
+const Title = styled.h1`
+    font-size: 2em;
+    color: rgb(0, 0, 0);
+`
+const Description = styled.p`
+    margin: 50px 0;
+    font-size: 20px;
+`
+
+const Button = styled.button`
+    background-color: #fafafa;
+    border: none;
+    `
+
 
 function Slider() {
   return (
@@ -67,10 +83,12 @@ function Slider() {
         <Wrapper>
             <Slide>
                 <ImgContainer>
-                    <Image src= "https://github.com/smainc/EcommerceSite/blob/main/images/womanJump.png" />
+                    <Image src= "https://i.ibb.co/4mKC0LV/woman-Smile.png" />
                 </ImgContainer>
                 <InfoContainer>
-                    A cute cat.
+                    <Title>SUMMER SALE</Title>
+                    <Description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam iste iure non odit minima, numquam exercitationem laboriosam facilis, reprehenderit perspiciatis quos, fugit fuga aliquam quod magni dolores eius eaque. Harum vel itaque provident ad atque, cupiditate tenetur iste ea corrupti deserunt pariatur illum perferendis eaque asperiores reiciendis nisi soluta labore sint porro aut. Mollitia iusto ipsam, delectus corrupti, amet autem omnis quidem aperiam quo qui corporis placeat natus perferendis? Libero amet necessitatibus ducimus, fugit asperiores tenetur dicta iusto in quo veritatis provident deserunt voluptatum harum? Placeat inventore eligendi id reprehenderit laborum cupiditate exercitationem, deleniti optio harum facilis provident nam sunt!</Description>
+                    <Button>Buy now</Button>
                 </InfoContainer>
             </Slide>
         </Wrapper>
