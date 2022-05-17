f0d732a1790ae4d0412064b9b979cfc888dbd1f7
smainc <40357673+smainc@users.noreply.github.com>
2022-05-17 09:13:15 -0700

    finished tiles

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     flex: 1;
-    margin: 0 10px;
-    border-radius: 5px;
-    overflow: hidden;
+    margin: 10px;
     background-color: #fefefe;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
     height: 70vh;
@@ -15,10 +13,25 @@ const Image = styled.img`
     height: 100%;
     background-color: #fefefe;
     object-fit: cover;
+    border-radius: 5px;
 `
 const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `
 const Title = styled.h3`
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: white;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
 `
 const Button = styled.button`
     background-color: #ff6b6b;
---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div``
+
+const Product = () => {
+    return (
+        <Container>
+        <div>Product</div>
+        </Container>
+    );
+}
+
+export default Product;
