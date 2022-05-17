2623666bf3916aee9f7ac28693ac4315cc066988
smainc <40357673+smainc@users.noreply.github.com>
2022-05-16 22:56:32 -0700

    added categories file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Announcements from '../components/Announcements'
 import Slider from '../components/Slider'
-import categories from '../components/categories'
+import Categories from '../components/Categories'
 const Home = () => {
   return (
     <div>
         <Announcements/>
         <Navbar/>
         <Slider/>
-        <categories/>
+        <Categories/>
     </div>
   )
 }
