ae63bbb51a1c6b606e48ce04d59ba8645a6af322
smainc <40357673+smainc@users.noreply.github.com>
2022-05-24 08:15:07 +0500

    slight change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Cart from "./pages/Cart.jsx";
 import Home from "./pages/Home.jsx";
+import Cart from "./pages/Cart.jsx";
 import Login from "./pages/Login.jsx";
 import Product from "./pages/Product.jsx";
 import ProductList from "./pages/ProductList.jsx";
